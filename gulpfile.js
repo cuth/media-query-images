d2761b647e30a766cb8564ad9628b216cddc6d1b
Jon Cuthbert <jon@cuth.net>
2014-06-01 13:04:28 -0400

    removed uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var insert = require('gulp-insert');
 var pjson = require('./package.json');
@@ -11,26 +10,19 @@ var BANNER = "\
  *  @preserve\n\
  */\n";
 
-var AMD_HEAD      = "define(['jquery'], function ($) {\n    'use strict';\n\n";
-var AMD_FOOT      = "\n\n});";
+var AMD_HEAD    = "define(['jquery'], function ($) {\n    'use strict';\n\n";
+var AMD_FOOT    = "\n\n});";
 
-var COMMON_HEAD   = "module.exports = (function ($) {\n    'use strict';\n\n";
-var COMMON_FOOT   = "\n\n}(require('jquery')));";
+var COMMON_HEAD = "module.exports = (function ($) {\n    'use strict';\n\n";
+var COMMON_FOOT = "\n\n}(require('jquery')));";
 
-var GLOBAL_HEAD   = "\n/*exported MediaQueryImages */\n\nvar MediaQueryImages = (function ($) {\n    'use strict';\n\n";
-var GLOBAL_FOOT   = "\n\n}(jQuery || Zepto || ender || $));";
-
-var UGLIFY_OPTIONS = {
-    preserveComments: 'some'
-};
+var GLOBAL_HEAD = "\n/*exported MediaQueryImages */\n\nvar MediaQueryImages = (function ($) {\n    'use strict';\n\n";
+var GLOBAL_FOOT = "\n\n}(jQuery || Zepto || ender || $));";
 
 gulp.task('build-amd', function () {
     gulp.src('src/media-query-images.js')
         .pipe(rename('media-query-images.amd.js'))
         .pipe(insert.wrap(BANNER + AMD_HEAD, AMD_FOOT))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('media-query-images.amd.min.js'))
-        .pipe(uglify(UGLIFY_OPTIONS))
         .pipe(gulp.dest('dist'));
 });
 
@@ -38,21 +30,13 @@ gulp.task('build-commonjs', function () {
     gulp.src('src/media-query-images.js')
         .pipe(rename('media-query-images.common.js'))
         .pipe(insert.wrap(BANNER + COMMON_HEAD, COMMON_FOOT))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('media-query-images.common.min.js'))
-        .pipe(uglify(UGLIFY_OPTIONS))
         .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build-global', function () {
     gulp.src('src/media-query-images.js')
         .pipe(insert.wrap(BANNER + GLOBAL_HEAD, GLOBAL_FOOT))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('media-query-images.min.js'))
-        .pipe(uglify(UGLIFY_OPTIONS))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', ['build-amd', 'build-commonjs', 'build-global']);
-
-gulp.task('default', ['build']);
+gulp.task('default', ['build-amd', 'build-commonjs', 'build-global']);
